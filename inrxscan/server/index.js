e8dc57a2597eab7153c7f5b6ca4b5e42672c04de
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-08-05 15:31:44 +0530

    import env in inrxscan/server

--- inrxscan/server/index.js
+++ inrxscan/server/index.js
@@ -18,7 +18,8 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
-
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 const app = express();
 app.use(cors());
 app.use(express.json());
