a0d8c3751834eff792ac40a6e502cc58c31e6049
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-08-16 17:20:31 +0530

    fixed env location in prisma config file

--- packages/config/prisma.config.ts
+++ packages/config/prisma.config.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import { defineConfig } from "prisma/config";
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "../../.env" });
 
 
 export default defineConfig({
